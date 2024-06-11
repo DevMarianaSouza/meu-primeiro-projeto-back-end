9503a3a0d5fae02ed7dec2070b0d41994c5b50d3
DevMarianaSouza <mariana.tt.souza@gmail.com>
2024-06-11 11:42:48 -0300

    adiiona a primeira versão do meu projeto

---
+++ mulheres.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const router = express.Router()
+
+const app = express()
+const porta = 3333
+
+const horaLocal = [
+    {
+        nome: 'Mariana Souza', 
+        imagem: 'https://media.licdn.com/dms/image/D4D03AQH5n8hjv1FUew/profile-displayphoto-shrink_200_200/0/1714416269852?e=1723075200&v=beta&t=IUS4nAKKWaFrf_wTGlDSBTohTghK-O4HOAnsuqzXntg',
+        minibio: 'Desenvolvedora'
+    },
+    {
+        nome: 'Simara Conceição',
+        imagem: 'http://github.com/simaraconceicao.png',
+        minibio: 'Desenvolvedora e Instrutora'
+    },
+    {
+        nome: 'Iana Chan',
+        imagem: 'http://bit.ly/3JCXBqP',
+        minibio: 'Fundadora da PrograMaria'
+    }
+]
+
+function mostraMulheres(request, response) {
+    response.json(mulheres)
+}
+
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta ", porta)
+}
+
+app.use(router.get('/mulheres', mostraMulheres))
+app.listen(porta, mostraPorta)
